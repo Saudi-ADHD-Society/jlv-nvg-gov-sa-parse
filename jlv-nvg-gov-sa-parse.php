6634e4805b032cd161cdbcfc0c8021e27adc369d
Jeremy Varnham <jeremy.varnham@gmail.com>
2022-07-26 05:22:12 +0000

    Moved filtering into function

--- jlv-nvg-gov-sa-parse.php
+++ jlv-nvg-gov-sa-parse.php
@@ -3,7 +3,7 @@
 Plugin Name: Saudi ADHD Society NVG.gov.sa parser
 Plugin URI: https://github.com/Saudi-ADHD-Society/jlv-nvg-gov-sa-parse
 Description: Fetches our latest volunteer opportunities from the Saudi National Volunteering portal
-Version: 1.0.1
+Version: 1.0.2
 Author: Jeremy Varnham
 Author URI: https://abuyasmeen.com
 License: GPL3
@@ -27,37 +27,23 @@ function jlv_nvg_fetch_shortcode( $atts="" ) {
 	$domxpath = jlv_nvg_get_dom_data( $full_url );
 	
 	// Paragraph element classes on NVG page.
-	$classnames['card_title']    = 'عنوان الفرصة';
-	$classnames['card_location'] = 'المدينة';
-	$classnames['card_text']     = 'وصف الفرصة';
-	$classnames['days_number']   = 'عدد الأيام المتبقية';
-	$classnames['dates']         = 'التواريخ';
-	$classnames['seats_number']  = 'عدد المقاعد';
-		
-	// Just keep the html elements with above class names.
-	foreach ( $classnames as $class => $label ) {
-		$expression     = './/p[contains(concat(" ", normalize-space(@class), " "), " ' . $class . ' ")]';
-		$elements       = $domxpath->evaluate( $expression );
-		$elements_count = $elements->count();
-		
-		foreach ( $elements as $element ) {
-			$opportunity[$class][] =  $element->nodeValue;
-		}
-	}
+	$classnames_p['card_title']    = 'عنوان الفرصة';
+	$classnames_p['card_location'] = 'المدينة';
+	$classnames_p['card_text']     = 'وصف الفرصة';
+	$classnames_p['days_number']   = 'عدد الأيام المتبقية';
+	$classnames_p['dates']         = 'التواريخ';
+	$classnames_p['seats_number']  = 'عدد المقاعد';
+	$classnames_a['join_btn']      = 'الرابط';
+	$classnames                    = array_merge( $classnames_p, $classnames_a );
 	
-	// Hyperlink element.
-	$linkclass = 'join_btn';
-	$classnames[$linkclass]       = 'الرابط';
+	$filtered_results_p = jlv_nvg_filter_html( $domxpath, 'p', $classnames_p, null );
+	$filtered_results_a = jlv_nvg_filter_html( $domxpath, 'a', $classnames_a, $site_url );
 	
-	$expression = './/a[contains(concat(" ", normalize-space(@class), " "), " ' . $linkclass . ' ")]';
-	$elements   = $domxpath->evaluate( $expression );
-	
-	foreach ( $elements as $element ) {
-		$opportunity[$linkclass][] =  '<a target="_blank" href="' . $site_url . $element->getAttribute('href') . '">' . $element->nodeValue . '</a>';
-	}
-	
+	$filtered_results   = array_merge( $filtered_results_p['html'], $filtered_results_a['html'] );
+	$elements_count     = $filtered_results_p['count'] + $filtered_results_a['count'];
+
 	// Construct output table.
-	$table = '<table>';
+	$table  = '<table>';
 	$table .= '<thead><tr>';
 	
 	foreach ( $classnames as $class => $label ) {
@@ -70,7 +56,7 @@ function jlv_nvg_fetch_shortcode( $atts="" ) {
 	for ( $i = 0; $i < $elements_count; $i++ ) {
 		$table .= '<tr>';
 		foreach ( $classnames as $class => $label ) {
-			$table .= '<td class="' . $class . '">' . $opportunity[$class][$i] . '</td>';
+			$table .= '<td class="' . $class . '">' . $filtered_results[$class][$i] . '</td>';
 		}
 		$table .= '</tr>';
 	}
@@ -117,4 +103,23 @@ function jlv_nvg_get_dom_data( $full_url ) {
 	$domxpath = new DomXPath( $dom );
 	
 	return $domxpath;
+}
+
+/*
+ * Filter the html elements by class.
+ *
+ */
+function jlv_nvg_filter_html( $domxpath, $html_element, $classnames, $site_url=null ) {
+	foreach ( $classnames as $class => $label ) {
+		$expression     = './/' . $html_element . '[contains(concat(" ", normalize-space(@class), " "), " ' . $class . ' ")]';
+		$elements       = $domxpath->evaluate( $expression );
+		$elements_count = $elements->count();
+		
+		foreach ( $elements as $element ) {
+			$filtered[$class][] = ( $html_element == 'a' ) ? $result[$class][] =  '<a target="_blank" href="' . $site_url . $element->getAttribute('href') . '">' . $element->nodeValue . '</a>' : $element->nodeValue;
+		}
+	}
+	
+	$result = array( 'html' => $filtered, 'count' => $elements_count );
+	return $result;
 }
